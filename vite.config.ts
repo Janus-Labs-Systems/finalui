3e1f518f9e347dfb670711d21a8060c8abb49c19
JanusLabs <shantanu@janussystems.xyz>
2026-06-05 17:39:51 +0530

    fix 4

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,31 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': [
+            'react',
+            'react-dom',
+            'react-router-dom',
+            '@mui/material',
+            '@mui/icons-material',
+          ],
+          'vendor-emotion': [
+            '@emotion/react',
+            '@emotion/styled',
+          ],
+          'vendor-tabulator': [
+            'tabulator-tables',
+            'react-tabulator',
+          ],
+          'vendor-axios': [
+            'axios',
+          ],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 600,
+  },
 })
